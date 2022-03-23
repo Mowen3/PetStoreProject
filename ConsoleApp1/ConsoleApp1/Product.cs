c60739ef6c120ce0d5cc95044a70c9ec72947209
Tyler Bumpus <tbumpus13@gmail.com>
2022-03-23 14:33:53 -0400

    first commit of created classes

---
+++ ConsoleApp1/ConsoleApp1/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Product//should this be a private class?
+    {
+        //should any of these be private?
+        public string Name { get; set; }
+        public Category Category { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
