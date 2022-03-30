1416da3dd612b743023a6b61f0969fc623f13b96
Malik El Guermai <malikelguermai@outlook.com>
2022-03-30 18:45:34 -0400

    Improvments

--- ConsoleApp1/ConsoleApp1/Payment.cs
+++ ConsoleApp1/ConsoleApp1/Payment.cs
@@ -18,22 +18,33 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Please enter the amount in cash you are paying with:");
             var tenderedCash = Convert.ToInt32(Console.ReadLine());
-            var change = grandTotal - tenderedCash;
+            var change = tenderedCash - grandTotal;
             return change ;
         }
 
+        //might be broken up
         public void PayWithCard()
         {
+            //number should be string
             Console.WriteLine("Please enter your credit card number:");
             CardNumber = int.Parse(Console.ReadLine());
+
+            //add private method for (helper method)
+            //date time . parse
+            //compare datetime now
             Console.WriteLine("Please enter the card expiration date in the format: MMYYYY");
             Expiration = int.Parse(Console.ReadLine());
+
+            //add private method for (helper method)
+            //should be a string
+            //check num and right length
             Console.WriteLine("Please enter the CVV code on the back of your card");
             CVV = int.Parse(Console.ReadLine());
         }
 
         public void PayWithCheck()
         {
+            //should be a string
             Console.WriteLine("Please enter your check number:");
             var checkNumber = int.Parse(Console.ReadLine());
         }
